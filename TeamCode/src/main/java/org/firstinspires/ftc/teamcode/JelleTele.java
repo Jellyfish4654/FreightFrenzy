25223338da45b5831d5fdbfc7037c0d07935b7f8
Allen B <pingguo.ny@gmail.com>
2022-02-01 00:39:21 -0500

    create BaseOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JelleTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JelleTele.java
@@ -1,19 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//import org.firstinspires.ftc.teamcode.framework.BaseOpMode;
+import org.firstinspires.ftc.teamcode.framework.BaseOpMode;
 import org.firstinspires.ftc.teamcode.framework.Motors;
 import org.firstinspires.ftc.teamcode.framework.components.Spinner;
 import org.firstinspires.ftc.teamcode.framework.components.Claw;
 
 @TeleOp(name = "UltimateGoal JelleTele")
-public class JelleTele extends LinearOpMode {
+public class JelleTele extends BaseOpMode {
     protected static enum DriveMode {
         TANK,
         DRIVE,
@@ -21,28 +20,6 @@ public class JelleTele extends LinearOpMode {
     }
 
     protected DriveMode driveMode = DriveMode.MECANUM;
-
-    protected DcMotor[] motors;
-    protected Claw claw;
-    protected Spinner spinner;
-    protected void initHardware() {
-        motors = new DcMotor[] {
-            hardwareMap.dcMotor.get("motor fr"),
-            hardwareMap.dcMotor.get("motor br"),
-            hardwareMap.dcMotor.get("motor fl"),
-            hardwareMap.dcMotor.get("motor bl")
-        };
-
-        motors[Motors.FR].setDirection(DcMotorSimple.Direction.REVERSE);
-        motors[Motors.BR].setDirection(DcMotorSimple.Direction.REVERSE);
-
-        CRServo carousel = hardwareMap.crservo.get("carousel");
-        spinner = new Spinner(carousel);
-
-        DcMotor clawPivot = hardwareMap.dcMotor.get("claw-pivot");
-        Servo clawServo = hardwareMap.servo.get("claw-servo");
-        claw = new Claw(clawPivot, clawServo);
-    }
 
     @Override
     public void runOpMode() throws InterruptedException {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/framework/BaseOpMode.java
@@ -1,7 +1,35 @@
 package org.firstinspires.ftc.teamcode.framework;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 
-abstract class BaseOpMode extends LinearOpMode {
+import org.firstinspires.ftc.teamcode.framework.Motors;
+import org.firstinspires.ftc.teamcode.framework.components.Spinner;
+import org.firstinspires.ftc.teamcode.framework.components.Claw;
 
+public abstract class BaseOpMode extends LinearOpMode {
+    protected DcMotor[] motors;
+    protected Claw claw;
+    protected Spinner spinner;
+    protected void initHardware() {
+        motors = new DcMotor[] {
+            hardwareMap.dcMotor.get("motor fr"),
+            hardwareMap.dcMotor.get("motor br"),
+            hardwareMap.dcMotor.get("motor fl"),
+            hardwareMap.dcMotor.get("motor bl")
+        };
+
+        motors[Motors.FR].setDirection(DcMotorSimple.Direction.REVERSE);
+        motors[Motors.BR].setDirection(DcMotorSimple.Direction.REVERSE);
+
+        CRServo carousel = hardwareMap.crservo.get("carousel");
+        spinner = new Spinner(carousel);
+
+        DcMotor clawPivot = hardwareMap.dcMotor.get("claw-pivot");
+        Servo clawServo = hardwareMap.servo.get("claw-servo");
+        claw = new Claw(clawPivot, clawServo);
+    }
 }
